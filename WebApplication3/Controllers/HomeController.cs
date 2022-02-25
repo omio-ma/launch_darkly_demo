6fcb3a35a76b3fd1beed1e8cab835ca73befc161
muktadiru <muktadiru.ahmed@google.com>
2022-02-25 16:25:04 +0000

    initial commit

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LaunchDarkly.Sdk.Server;
+using LaunchDarkly.Sdk.Server.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ILdClient _ldClient;
+
+        public HomeController(ILogger<HomeController> logger, ILdClient ldClient)
+        {
+            _logger = logger;
+            _ldClient = ldClient;
+        }
+
+        public IActionResult Index()
+        {
+            //var user = LaunchDarkly.Sdk.User.WithKey("api-user-key");
+            var user = LaunchDarkly.Sdk.User.WithKey("asdsad");
+            var flags = _ldClient.AllFlagsState(user).ToValuesJsonMap();
+            var featureval = _ldClient.BoolVariation("feature-one", user);
+            var featureval2 = _ldClient.BoolVariation("admissions", user);
+
+            return View(new HomeViewModel { FeatureOne = featureval, FeatureTwo = featureval2});
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication3/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class HomeViewModel
+    {
+        public bool FeatureOne { get; set; }
+        public bool FeatureTwo { get; set; }
+    }
+}
